9054a8309514361e9ae055be49745e0f57d273c9
Logan Olson <loganolsonn@gmail.com>
2025-02-03 16:47:47 -0600

    Fixed deployment

--- components/experiences.tsx
+++ components/experiences.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Link2, ChevronRight } from 'lucide-react';
+import { Link2 } from 'lucide-react';
 import { experienceData } from '@/lib/data';
 
 type ExperienceProps = (typeof experienceData)[number];
